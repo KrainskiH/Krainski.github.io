13a5b2e4411aae584a7a08c2335b273b3ce0a7f1
Krainski H <henriquekrainski38@gmail.com>
2026-01-13 11:49:30 -0300

    finalizando 1 etapa portifolio

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ window.addEventListener("scroll", () => {
     }
   });
 });
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(e => {
+    if (e.isIntersecting) e.target.classList.add("active");
+  });
+});
+
+document.querySelectorAll(".reveal").forEach(el => observer.observe(el));
+document.querySelectorAll(".progress-bar").forEach(bar => {
+  const value = bar.getAttribute("data-value");
+  bar.style.width = value + "%";
+});
